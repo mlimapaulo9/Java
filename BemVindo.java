54b28c817f52a8a748c9b5d48509997cc96f4ac0
Marcus Paulo <mlima.paulo9@gmail.com>
2018-07-12 21:31:57 -0300

    Cap 3 - netbeans
    Capitulo 3 do livro e instalação do netbeans

---
+++ BemVindo.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class BemVindo
+{
+	public static void main(String[] args)
+	{
+
+		JOptionPane.showMessageDialog(null,"Bem-vindo ao Java");
+		System.exit(0);
+	}
+}
---
+++ JavaApplication1/src/Saida.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author marcus
+ */
+public class Saida {
+    public static void main(String[] args)
+    {
+        System.out.println( );
+        
+        System.out.print("Um Caractere:\t\t");
+        System.out.print('A');
+        System.out.print("\n");
+        
+        System.out.print("Um numero Inteiro:\t");
+        System.out.print(123);
+        System.out.print("\n");
+        
+        System.out.println("Um numero decimal:\t" + 4.3);
+        System.out.println("Um valor booleano:\t" + true);
+        System.out.println("Uma barra invertida:\t\\");
+        System.out.println("Um texto entre aspas:\t"+"\"Marcus\"");
+        
+        System.out.println( );
+        
+    
+    }
+}
